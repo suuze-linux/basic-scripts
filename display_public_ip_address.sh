e3c52177b2d9925051abb98152aaac84eb133406
suuze-linux <91548585+suuze-linux@users.noreply.github.com>
2025-02-01 11:37:48 +0200

    Update display_public_ip_address.sh
    Updated description in comments to make it clear that script relies on the existence of an AWS service.

--- display_public_ip_address.sh
+++ display_public_ip_address.sh
@@ -3,7 +3,9 @@
 # Simple bash script to display the public IP address currently in use
 # by the invoking machine. 
 #
-# Uses AWS' checkip service and assumes curl is installed in /usr/bin/.
+# Relies on:
+# - AWS' checkip service at https://checkip.amazonaws.com/, and
+# - assumes curl is installed in /usr/bin/.
 #
 
 /usr/bin/curl https://checkip.amazonaws.com/
